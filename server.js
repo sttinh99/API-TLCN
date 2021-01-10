1aba635d3afc944e12fae9abdae09ceabe66d4a2
sttinh99 <truongtinh110399@gmail.com>
2021-01-10 10:43:46 +0700

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const user = require('./routers/user.router')
 const category = require('./routers/categories.router');
@@ -47,6 +48,12 @@ app.get('/', (req, res) => {
     res.json('test');
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
